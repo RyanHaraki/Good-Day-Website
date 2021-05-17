e37e5a2c1135d7c2bc46da0e56db17c7c720d79d
Ryan Haraki <54621020+RyanHaraki@users.noreply.github.com>
2021-05-17 01:03:29 -0700

    remove client side form validation

--- public/js/index.js
+++ public/js/index.js
@@ -2,16 +2,16 @@ const email = document.getElementById("email").value;
 const form = document.querySelector("#inputbox");
 
 form.addEventListener("submit", (e) => {
-  let messages = [];
+//   let messages = [];
 
-  if (email === "" || email == null) {
-    messages.push("Please enter your email");
+//   if (email === "" || email == null) {
+//     messages.push("Please enter your email");
 
-  }
+//   }
 
-  if (messages.length > 0) {
-    e.preventDefault();
-    alert(messages.join(","));
-    console.log(messages)
-  }
+//   if (messages.length > 0) {
+//     e.preventDefault();
+//     alert(messages.join(","));
+//     console.log(messages)
+//   }
 });
